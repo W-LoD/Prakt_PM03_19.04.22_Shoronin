e4a68937e0fd35ab25051f22321c5526e36167a8
stu-pksp118 <stu-pksp118@AMDVLA3511GW09>
2022-04-19 09:40:16 +0300

    Запрос размера массива N, создание класса TouristicOperator, создание массива

--- src/Program.cs
+++ src/Program.cs
@@ -10,12 +10,36 @@ namespace src
     {
         static void Main(string[] args)
         {
+            int N = 0;
+            Console.WriteLine("Введите размер массива N= ");
+            try
+            {
+                N = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Размер массива должен быть числом больше 0!");
+            }
+            TouristicOperator touristicOperator = new TouristicOperator();
+            touristicOperator.Trips = new Trip[N];
+            
         }
+
+            
+            
+        
     }
     public class Trip
     {
         public int Duration;
         public int Price;
         public int Size_of_the_tour_group;
+    }
+    public class TouristicOperator
+    {
+        public Trip[] Trips;
+
+        
     }
 }
