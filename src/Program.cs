75a8fbf74441fd699b2acbc7d6bcd70cadd63671
stu-pksp118 <stu-pksp118@AMDVLA3511GW09>
2022-04-19 09:55:44 +0300

    Добавление сортировки и вывода на экран отсортированного массива для проерки работоспособности соритровки

--- src/Program.cs
+++ src/Program.cs
@@ -23,7 +23,21 @@ namespace src
             }
             TouristicOperator touristicOperator = new TouristicOperator();
             touristicOperator.Trips = new Trip[N];
-            
+            Console.WriteLine("Введите элементы массива(Продолжительность поездки в днях, Цену поездки, Размер туристической группы)");
+            for (int i = 0; i < N; i++)
+            {
+                touristicOperator.Trips[i] = new Trip();
+                touristicOperator.Trips[i].Duration = int.Parse(Console.ReadLine());
+                touristicOperator.Trips[i].Price = int.Parse(Console.ReadLine());
+                touristicOperator.Trips[i].Size_of_the_tour_group = int.Parse(Console.ReadLine());
+            }
+            touristicOperator.Sort_mas(touristicOperator.Trips);
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("\n" + touristicOperator.Trips[i].Duration + " "
+                    + touristicOperator.Trips[i].Price + " "
+                    + touristicOperator.Trips[i].Size_of_the_tour_group);
+            }
         }
 
             
@@ -40,6 +54,21 @@ namespace src
     {
         public Trip[] Trips;
 
-        
+        public void Sort_mas(Trip[] Trips)
+        {
+            Trip temp;
+            for (int i = 0; i < Trips.Length - 1; i++)
+            {
+                for (int j = i + 1; j < Trips.Length; j++)
+                {
+                    if (Trips[i].Price > Trips[j].Price && Trips[i].Size_of_the_tour_group > Trips[j].Size_of_the_tour_group)
+                    {
+                        temp = Trips[i];
+                        Trips[i] = Trips[j];
+                        Trips[j] = temp;
+                    }
+                }
+            }
+        }
     }
 }
